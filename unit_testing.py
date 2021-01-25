aa2b96da1625e0b0f95432b66779677e3da522dc
Nathaniel Ortiz <thelectronicnub@gmail.com>
2021-01-24 18:46:57 -0800

    Refactor and add odometry node

--- unit_testing.py
+++ unit_testing.py
@@ -1,8 +1,14 @@
+#https://docs.python.org/3/library/unittest.html
+#http://wiki.ros.org/unittest
+#https://pythontesting.net/framework/unittest/unittest-introduction/
+#http://docs.ros.org/en/diamondback/api/nav_msgs/html/msg/Odometry.html odometry node format
+
 #!/usr/bin/env python
 PKG = 'test_roslaunch'
 import roslib; roslib.load_manifest(PKG)  # This line is not needed with Catkin.
- 
-import sys, unittest,time, random
+import rospy
+from nav_msgs.msg import Odometry
+import sys, unittest, time, random
 import numpy as np
 
 def genWaypoints(num_waypoints):
@@ -17,16 +23,28 @@ def genWaypoints(num_waypoints):
         
         random_xyz = np.array([[x,y,z]])
         np.append(waypoints,[random_xyz],axis=0)
+        
+    return waypoints
+    
+def checkWaypoints(next_waypoint)
+
+    current_waypoint = self.pose
+    
+    move_to_point(self, current_waypoint, next_waypoint)
+    current_waypoint = self.pose
+    
+    return current_waypoint
 
 class TestTrajectory(unittest.TestCase):
+    rospy.init_node('check_odometry')
+    odom_sub = rospy.Subscriber('/odom', Odometry, callback)
+
 
      def test_move_to_point(self):
-         self.assertEquals(move_to_point(self, current_waypoint, next_waypoint), next_waypoint)
+        for i in range(waypoint):
+         self.assertAlmostEquals(checkWaypoints(), self.pose)
  
  if __name__ == '__main__':
      import rostest
      rostest.rosrun(PKG, 'test_move_to_point', TestTrajectory)
      
-#https://docs.python.org/3/library/unittest.html
-#http://wiki.ros.org/unittest
-#https://pythontesting.net/framework/unittest/unittest-introduction/
