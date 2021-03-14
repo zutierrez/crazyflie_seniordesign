97b18856fa15365dc2e761b092785aeea63ffbfb
Nathaniel Ortiz <thelectronicnub@gmail.com>
2021-03-14 13:33:10 -0700

    Add task gen testing and set tolerance for waypoints

--- unit_testing.py
+++ unit_testing.py
@@ -10,11 +10,13 @@ import rospy
 from nav_msgs.msg import Odometry
 import sys, unittest, time, random
 import numpy as np
+import allocTask
+import taskGenerator
 
-def genWaypoints(num_waypoints):
+def genWaypoints(num_waypoints): #randomly generates waypoints
     random.seed(time.time())
     
-    waypoints = np.empty(0)
+    waypoints = np.empty(num_waypoints)
     
     for i in range(num_waypoints):
         x = random.randrange(1,100,1)
@@ -28,12 +30,28 @@ def genWaypoints(num_waypoints):
     
 def checkWaypoints(next_waypoint)
 
-    current_waypoint = self.pose
+    current_waypoint = self.pose #update waypoint to current pos
     
-    move_to_point(self, current_waypoint, next_waypoint)
-    current_waypoint = self.pose
+    move_to_point(self, current_waypoint, next_waypoint) #move to next waypoint
+    current_waypoint = self.pose #update current waypoint to pos
     
-    return current_waypoint
+    return current_waypoint #return current position
+    
+def genTasks(num_tasks):
+    robotParams = [ #define robot params as defined in taskGenerator
+    'robotParamOne',
+    'robotParamTwo',
+    'robotParamThree'
+    ]
+
+    random.seed(time.time())
+    
+    tasks = np.empty(num_tasks)
+    
+    for i in range(num_tasks):
+        tasks[i] = allocTask.task('task ' i, random.randrange(0,10), random.randrange(0,10), 1, robotParams = random.sample(range(3),3), random.randrange(1,5) * 5) #randomly generate tasks
+        
+    return tasks
 
 class TestTrajectory(unittest.TestCase):
     rospy.init_node('check_odometry')
@@ -42,9 +60,25 @@ class TestTrajectory(unittest.TestCase):
 
      def test_move_to_point(self):
         for i in range(waypoint):
-         self.assertAlmostEquals(checkWaypoints(), self.pose)
+         self.assertAlmostEquals(checkWaypoints(), self.pose, 0.05) #check that the waypoint is equal to current drone pos, +/- 5%
  
  if __name__ == '__main__':
      import rostest
      rostest.rosrun(PKG, 'test_move_to_point', TestTrajectory)
      
+class TestTaskAlloc(unittest.TestCase):
+    tasks = genTasks(20) #Generate 20 sample tasks
+
+    for i in range(tasks): 
+        print(tasks(i)) #print each task generated
+        
+    def test_task_alloc(self):
+        for i in range(tasks):
+            ranked_output = mrta_rank(tasks[i]) #begin ranking
+            print(ranked_output)
+            task_alloc(ranked_output,tasks[i]) #allocate tasks
+            self.assertEquals(tasks[i], ranked_output) #ensure the ranked task is equivalent to its proper rank
+        
+ if __name__ == '__main__':
+     import rostest
+     rostest.rosrun(PKG, 'test_task_alloc', TestTaskAlloc)
